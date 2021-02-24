6977481b8fd2e30a87c24406dc01849d30f3d524
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-02-24 11:43:47 -0800

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express'); //requiring express up here
+
+
+const app = express(); ///instantiating the server here
+
+const myNotes = require('./db/db'); ///maybe i dont need to format it like { myNotes }???
+
+app.get('/api/db', (req, res) => {
+  //  res.json(myNotes); /// can see data at http://localhost:3001/api/db
+    //res.send('testing')
+    let results = myNotes
+    //console.log(req.query)
+    res.json(results)
+})
+
+app.get('/api/db/:noteTitle', (req, res)=> {
+    const id = req.params.noteTitle
+    let myNote = myNotes[id]
+    res.json(myNote) ///example URL: http://localhost:3001/api/db/2
+})
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
+//heroku link: https://limitless-coast-45003.herokuapp.com/ 
