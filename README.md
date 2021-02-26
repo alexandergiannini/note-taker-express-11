c8ff5f070763417f5b8b1fca0d6c7ad992b9f450
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-02-25 21:57:29 -0800

    added JS edits and a full readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Note Taker Starter Code
+# Note Taker Application
 
-# Hello there, my name is Alexander Giannini and this is my Note Taker Application!
+# Hello there, my name is Alexander Giannini and this is my Note Taker Application! With this application, you can see existing notes, make a new note, edit existing notes, and delete notes. The backend of this application is powered by Express.js, which made all of this possible. Hope you enjoy!
+
+# Link to application: https://limitless-coast-45003.herokuapp.com/
+
+# Screenshot of application: <img width="1730" alt="Screen Shot 2021-02-25 at 9 52 27 PM" src="https://user-images.githubusercontent.com/74731953/109260847-c1d8e300-77b3-11eb-89c2-1f043c346d61.png">
--- server.js
+++ server.js
@@ -3,90 +3,94 @@ const express = require('express'); //requiring express up here
 const PORT = process.env.PORT || 3001;
 const app = express(); ///instantiating the server here
 
-const path = require('path')
+const path = require('path'); //requiring the path here
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-app.use(express.static('public')); //using this to access other files in public folder (namely index.js)
+app.use(express.static('public')); //using this to access other files in public folder
 
 
 const myNotes = require('./db/db'); ///maybe i dont need to format it like { myNotes }???
 
+///function that filters query search within the URL/notes page.
 function filterByQuery (query, notesArray) {
-    let filteredResults = notesArray
+    let filteredResults = notesArray;
 
     if (query.title) {
-        filteredResults = filteredResults.filter(note => note.title === query.title) ///this takes account the title propery on db.json file
+        filteredResults = filteredResults.filter(note => note.title === query.title); ///this takes account the title propery on db.json file
     }
 
     if (query.text) {
-        filteredResults = filteredResults.filter(note => note.text === query.text) //////this takes account the text propery on db.json file
+        filteredResults = filteredResults.filter(note => note.text === query.text); //////this takes account the text propery on db.json file
     }
 
-    return filteredResults 
+    return filteredResults ;
 
     ////http://localhost:3001/api/notes?title=Erica
     //http://localhost:3001/api/notes?title=Test%20Title
     ///http://localhost:3001/api/notes?text=Test%20text
 }
 
+
+//not used so far...
 function createNewNote (body, notesArray) {
-    console.log(body)
+    console.log(body);
 
 
 
-    return body
+    return body;
 }
 
+/// creating a get html path to the index.html page
 app.get('/', (req, res) => {
     res.sendFile(
         path.join(__dirname, './public/index.html') ///http://localhost:3001/
     )
 })
 
+/// creating a get html path here to the notes.html page
 app.get('/notes', (req, res) => {
     res.sendFile(
         path.join(__dirname, './public/notes.html')
     )
 })
 
-
+// creating an api route which reads the db.json file and returns all the notes saved as a JSON
 app.get('/api/notes', (req, res) => {
-  //  res.json(myNotes); /// can see data at http://localhost:3001/api/notes
-    //res.send('testing')
-    let results = myNotes
+ 
+    let results = myNotes;
    
     if (req.query) {
-        results = filterByQuery(req.query, results)
+        results = filterByQuery(req.query, results);
     }
-    res.json(results)
+    res.json(results);
 })
 
+// creating an api route which displays in ID(?) within the URL
 app.get('/api/notes/:noteTitle', (req, res)=> {
-    const id = req.params.noteTitle
-    let myNote = myNotes[id]
-    res.json(myNote) ///example URL: http://localhost:3001/api/notes/1
+    const id = req.params.noteTitle;
+    let myNote = myNotes[id];
+    res.json(myNote); ///example URL: http://localhost:3001/api/notes/1
 })
 
+
+/////// !!! i need to be able to store an ID here when saving a new note
+//POST api route which receives a new note to save and displays it on the db.json and returns the new note to the client
 app.post('/api/notes', (req, res) => {
      // req.body is where our incoming content will be
   //console.log(req.body); //req.body property is where our incoming content will be.
   //req.body.id = myNotes.length.toString();
 
-  /////// i need to be able to store an ID here when saving a new note
-
-  let newNote = req.body
-  myNotes.push(newNote)
-  console.log(newNote)
-  res.json(myNotes)
-
-  //res.json(req.body);
+  let newNote = req.body;
+  myNotes.push(newNote);
+  console.log(newNote);
+  res.json(myNotes);
 })
 
-
+///app listen method
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
   });
